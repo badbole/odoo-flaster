78ba23fd4bc706142f60a048d28d6a1628b9777c
Davor Bojkić - Bole <bole@dajmi5.com>
2023-01-20 11:21:35 +0100

    [add] a little db backup helper - enable users to see lovaly available backup files todo: imporoove this functionality

---
+++ auto_backup_file_manager/__manifest__.py
@@ -0,0 +1,18 @@
+
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Auto backup - file manager",
+    "version": "16.0.1.0.0",
+    "author": "Daj mi 5",
+    "category": "Tools",
+    "website": "https://github.com/OCA/server-tools",
+    "license": "AGPL-3",
+    "summary": "Backups file manager",
+    "depends": [
+        'auto_backup',
+    ],
+    'data': [
+        "views/db_backup_views.xml"
+    ],
+    "installable": True,
+}
